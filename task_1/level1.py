4b834de1940404d42c66beb94de2ccd49dc47a97
Mastewal <mastewalgshiferaw@gmail.com>
2026-04-07 13:49:49 +0300

    update reverse string code

--- task_1/level1.py
+++ task_1/level1.py
@@ -1,6 +1,7 @@
 def reverse_str(text):
     #returns the reverse version of a string
-    return text[::1]
+    return text[::-1]
+    #text[start : stop : step]
 
 #example
 print(reverse_str("Python"))
